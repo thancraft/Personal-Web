186097c4762faaf6b6773202303889a0ef24fecf
Ahmad Fadli <thancraft313@gmail.com>
2025-10-02 12:21:02 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,11 @@ import WelcomeScreen from "./Pages/WelcomeScreen";
 import { AnimatePresence } from 'framer-motion';
 import notfound from "./Pages/404";
 import NotFoundPage from "./Pages/404";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+
+<template>
+  <SpeedInsights />
+</template>
 
 const LandingPage = ({ showWelcome, setShowWelcome }) => {
   return (
